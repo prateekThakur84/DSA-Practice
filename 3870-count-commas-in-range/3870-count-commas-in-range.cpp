07ab80c99d279234571e4c3358b0d28a4aa2c5a9
Prateek Thakur <142097726+prateekThakur84@users.noreply.github.com>
2026-09-08 09:53:07 +0530

    Time: 1 ms (16.95%), Space: 9.4 MB (5.52%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        string s = to_string(n);
+        int t = s.size();
+        if(t<4) return 0;
+        else return n-999;
+
+
+
+
+        
+        
+    }
+};
